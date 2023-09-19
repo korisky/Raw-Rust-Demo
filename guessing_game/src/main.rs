60060f7f60fb9f1134a2375b31827b3aff6e190e
roylic <roylic@wokoworks.com>
2023-09-19 17:08:33 +0800

    adding match -> for handling incorrect input (like a loop, also like using switch to handle different parsing result)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,33 +10,44 @@ fn main() {
     // start..=end is the pattern with lower & upper bound
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
 
-    // in Rust, variables are immutable by default -> let abc = 10, abc could not be changed
-    // if we want it to be mutable, need to use 'mut' before variable's name
-    // :: means that new() is an associative function of String type
-    let mut guess = String::new();
 
-    // we could use std::io::stdin if we not import std::io
-    io::stdin()
-        // & indicate it's a reference
-        // also, need to use '&mut guess' rather than '&guess' to make it mutable
-        .read_line(&mut guess)
-        // expect if for handling the error, would get a warning if missing
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    // convert String type to unsigned-32 bytes integer, the parse() func -> convert String to another type
-    // the : after guess, let Rust know we need to annotate
-    // Rust can allow use to 'Shadowing' the previous value with a new one, rather than create two unique variables
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        // in Rust, variables are immutable by default -> let abc = 10, abc could not be changed
+        // if we want it to be mutable, need to use 'mut' before variable's name
+        // :: means that new() is an associative function of String type
+        let mut guess = String::new();
 
-    // in Rust, {} is the placeholder
-    println!("You guessed: {guess}");
+        // we could use std::io::stdin if we not import std::io
+        io::stdin()
+            // & indicate it's a reference
+            // also, need to use '&mut guess' rather than '&guess' to make it mutable
+            .read_line(&mut guess)
+            // expect if for handling the error, would get a warning if missing
+            .expect("Failed to read line");
 
-    // Ordering is an Enum with 3 values -> match is little bit similar to case in switch
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Correct"),
+        // convert String type to unsigned-32 bytes integer, the parse() func -> convert String to another type
+        // the : after guess, let Rust know we need to annotate
+        // Rust can allow use to 'Shadowing' the previous value with a new one, rather than create two unique variables
+        let guess: u32 = match guess.trim().parse() {
+            // we could also use 'match' to handle parse() function's enum result
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // in Rust, {} is the placeholder
+        println!("You guessed: {guess}");
+
+        // Ordering is an Enum with 3 values -> match is little bit similar to case in switch
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Correct");
+                break;
+            }
+        }
     }
 }
