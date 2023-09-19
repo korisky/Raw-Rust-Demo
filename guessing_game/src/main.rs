cc367fb156eec13b54ef21a1cc22bb7a6942df74
roylic <roylic@wokoworks.com>
2023-09-19 16:05:40 +0800

    init file for the guessing name

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,24 @@
+// standard Rust library
+use std::io;
+
+// fn declare a new function
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // in Rust, variables are immutable by default -> let abc = 10, abc could not be changed
+    // if we want it to be mutable, need to use 'mut' before variable's name
+    // :: means that new() is an associative function of String type
+    let mut guess = String::new();
+
+    // we could use std::io::stdin if we not import std::io
+    io::stdin()
+        // & indicate it's a reference
+        // also, need to use '&mut guess' rather than '&guess' to make it mutable
+        .read_line(&mut guess)
+        // expect if for handling the error, would get a warning if missing
+        .expect("Failed to read line");
+
+    // in Rust, {} is the placeholder
+    println!("You guessed: {guess}")
 }
