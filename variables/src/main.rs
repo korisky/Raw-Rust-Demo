82d7b4c5d5f08b218d7f3eb940625deec924bd86
roylic <roylic@wokoworks.com>
2023-09-22 16:27:03 +0800

    by using let keyword, we could also easily change the variable's type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -11,8 +11,13 @@ fn main() {
     let y = 5;
     let y = y + 1;
     {
-        let y = y * 2;
+        let y = 2.0 * 2.5;
         println!("The value of y in the inner scope is: {y}");
     }
+    // by using let -> after the inner scope, the value would result back
     println!("The value of y in the outer scope is: {y}");
+
+    // also, by using let -> we can even change the original type of a value
+    let y = 4.5;
+    println!("The value of y now become: {y}");
 }
