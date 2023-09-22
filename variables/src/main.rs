d8783cb4c7802f70845e328f6e0742c76b194e1f
roylic <roylic@wokoworks.com>
2023-09-22 15:58:59 +0800

    adding constants keyword checking -> in RustRover, we need to 'attach cargo project' like adding mvn project manually

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    println!("The const: {THREE_HOURS_IN_SECONDS}");
 }
