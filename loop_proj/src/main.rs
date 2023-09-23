fab6f8a562adc14bf415c557ad55cfc3555cb37d
roylic <lykismekk@outlook.com>
2023-09-23 14:55:16 +0800

    for Rust's loop, we can name a loop with '_name_: loop{_logics_}, then with break _name_, we can jump out of that loop, even we are in the inner loop, crazy multi loop feature, great for geeks

--- loop_proj/src/main.rs
+++ loop_proj/src/main.rs
@@ -1,12 +1,38 @@
 fn main() {
-    let mut counter = 0;
+    multi_loop_fn();
+}
 
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2 // multiply 2 then pass the return val
+// fn simple_loop_fn() {
+//     let mut counter = 0;
+//     let result = loop {
+//         counter += 1;
+//         if counter == 10 {
+//             break counter * 2; // multiply 2 then pass the return val
+//         }
+//     }; // assign the result of the loop to variable result (immutable)
+//     println!("The counter result is {result}");
+// }
+
+fn multi_loop_fn() {
+    let mut count = 0;
+
+    // here using '_name_: loop{...}, we set a name for this loop
+    // then inside of the loop, even we have embedded loops,
+    // we could determine which one we a breaking (i.e. jump out to the loop we want)
+    'counting_up: loop {
+        println!("\ncount = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 8 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up; // calling it's name, we could jump out of it
+            }
+            remaining -= 1;
         }
-    };
-
-    println!("The counter result is {result}");
+        count += 1;
+    }
+    println!("\nEnd count = {count}")
 }
