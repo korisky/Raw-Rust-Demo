9a7ead5d4399b7b3b2cd4d04506a4403d3f8c17e
roylic <roylic@wokoworks.com>
2023-09-25 18:17:26 +0800

    deep copy by Rust

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,11 +2,31 @@
 // - Each value in Rust has an owner.
 // - There can only be one owner at a time.
 // - When the owner goes out of scope, the value will be dropped.
+//
+// for gc, rust does not have, but
+// Rust takes a different path: the memory is automatically returned once the
+// variable that owns it goes out of scope.
 fn main() {
-    // mutable & dynamic size param
-    let mut s = String::from("Hello");
-    // append into String s
-    s.push_str(", Rust");
-    // way to print string
-    println!("{}", s)
+    {
+        // mutable & dynamic size param
+        let mut s = String::from("Hello");
+        // append into String s
+        s.push_str(", Rust");
+        // way to print string
+        println!("{}", s)
+    }
+    // out of the scope, Rust would call drop() automatically at the closing bracket
+
+    // for String, does not work
+    let s1 = String::from("S1");
+    // let s2 = s1; // not working, Rust compiler prevent on compiling
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    // but for integers, they works, because
+    // If a type implements the Copy trait, variables that use it do not 'move' (point to same slot),
+    // but rather are trivially copied, making them still valid after assignment to another variable
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
 }
