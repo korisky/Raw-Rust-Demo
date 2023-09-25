cfa718539b4d8e8aab949db0d403f14d5ba4bcff
roylic <roylic@wokoworks.com>
2023-09-25 18:05:35 +0800

    simple way on using String

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,12 @@
+// ownership principle:
+// - Each value in Rust has an owner.
+// - There can only be one owner at a time.
+// - When the owner goes out of scope, the value will be dropped.
 fn main() {
-    println!("Hello, world!");
+    // mutable & dynamic size param
+    let mut s = String::from("Hello");
+    // append into String s
+    s.push_str(", Rust");
+    // way to print string
+    println!("{}", s)
 }
