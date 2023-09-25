96cefa9e725e3d5b4686d2bcf6000d95110980ad
roylic <lykismekk@outlook.com>
2023-09-25 23:44:47 +0800

    add dangle stuff

--- ownership/mut_reference/src/main.rs
+++ ownership/mut_reference/src/main.rs
@@ -17,3 +17,16 @@ fn main() {
 fn change(some_string: &mut String) {
     some_string.push_str(", mutable reference");
 }
+
+// here is a dangle coding -> without consider the lifetime of function & value
+// when function returns, the value would be deallocated
+// fn dangle() -> &String {
+//     let s = String::from("dangle");
+//     &s
+// }
+
+// here is a no-dangled function, return the val mean pass the ownership
+fn no_dangle() -> String {
+    let s = String::from("dangle");
+    s
+}
