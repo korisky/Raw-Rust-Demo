3c7e47a8e6a24a5291b2625af27583360af883eb
roylic <roylic@wokoworks.com>
2023-09-25 18:32:14 +0800

    demonstration of Rust's ownership -> For complex type (e.g. String, Structs), after passing them to function, current function has no ownership it now, if we called it again, compiler would throw exception

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,6 +7,25 @@
 // Rust takes a different path: the memory is automatically returned once the
 // variable that owns it goes out of scope.
 fn main() {
+    let s = String::from("hello"); // s comes into scope
+    takes_ownership(s); // s's value moves into function, is no longer valid here
+    // println!("{}", s); // compiler would throw exception, let pass s.clone() instead
+
+    let x = 5; // x comes into scope
+    makes_copy(x); // x would move into the function, but i32 is Copy, x is still valid
+    println!("{}", x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+
+fn simple_collection() {
     {
         // mutable & dynamic size param
         let mut s = String::from("Hello");
