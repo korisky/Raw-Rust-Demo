a621f9a438759791eb21a5c5b31220c2fe05c150
roylic <roylic@wokoworks.com>
2023-09-26 15:41:22 +0800

    add simple struct using

---
+++ structs/simple_struct/src/main.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let mut usr = build_user(String::from("first@open.mail"), String::from("Name"));
+    usr.email = String::from("second@open.mail");
+    println!("{:?}", usr); // by setting structs entity mutable, the change take effect
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username, // we can use shorthand, change username:username to simply username
+        email,
+        sign_in_count: 1,
+    }
+}
