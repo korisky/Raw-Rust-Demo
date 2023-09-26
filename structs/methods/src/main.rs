e53b16d254fcc6271ce916c113d6a5207b373b64
roylic <roylic@wokoworks.com>
2023-09-26 17:30:20 +0800

    add simple multi-input method might relate to comparison stuff of same struct entites

--- structs/methods/src/main.rs
+++ structs/methods/src/main.rs
@@ -11,6 +11,11 @@ impl Rectangle {
         self.width * self.height
     }
 
+    // method compare with another entity
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
     fn pass_ownership(self) -> u32 {
         self.width
     }
@@ -23,6 +28,13 @@ fn main() {
     };
 
     println!("Square pixels: {}", rect1.area()); // here we can use method syntax to call the method
-    println!("The width: {}", rect1.pass_ownership());
+    println!("Square pixels: {}", &rect1.area()); // actually when you call rect1.area(), Rust compile it as &rect1.area()
+    // println!("The width: {}", rect1.pass_ownership());
     // println!("Call again: {}", rect1.width); // after passing the ownership, again, main() could not access rect1 again
+
+    println!();
+    let rect2 = Rectangle { width: 11, height: 1001 };
+    if rect2.can_hold(&rect1) {
+        println!("rect2 can hold rect1");
+    }
 }
