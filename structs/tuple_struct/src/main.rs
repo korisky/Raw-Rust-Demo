f2c48e2098889ac24c8eb51d0fe499a0327234a8
roylic <roylic@wokoworks.com>
2023-09-26 16:40:33 +0800

    by passing the struct input, it will make more sense for further maintainance

--- structs/tuple_struct/src/main.rs
+++ structs/tuple_struct/src/main.rs
@@ -1,19 +1,39 @@
 #[derive(Debug)]
 struct Color(i32, i32, i32);
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let black = Color(0, 0, 0);
     println!("{:?}", black);
     println!("{}", black.0);
 
-    // using the calling with tuple
+    // calling with tuple
     println!(
         "The area of the rectangle is {} square pixels.",
         area((30, 50))
     );
+
+    // calling with struct
+    let rect1 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect1)
+    );
 }
 
-// by
+// using tuple as input
 fn area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+// using structs' reference as input
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
 }
