669a7fc9f02d82015654c6c3c37fd58a4b2e9b6b
roylic <roylic@wokoworks.com>
2023-09-26 16:36:57 +0800

    by convert the fn input use tuple, it will make the function make more senses

--- structs/tuple_struct/src/main.rs
+++ structs/tuple_struct/src/main.rs
@@ -3,5 +3,17 @@ struct Color(i32, i32, i32);
 
 fn main() {
     let black = Color(0, 0, 0);
-    println!("{:?}", black)
+    println!("{:?}", black);
+    println!("{}", black.0);
+
+    // using the calling with tuple
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area((30, 50))
+    );
+}
+
+// by
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
