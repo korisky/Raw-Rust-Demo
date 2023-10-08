881d4bd55d79531a41a181413138f75fd1ee14b3
roylic <roylic@wokoworks.com>
2023-10-08 17:46:46 +0800

    correct path + match combined with enum

---
+++ enum_stuff/match_enum/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState), // as mentioned before, enum in Rust can be combined
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => { 1 }
+        Coin::Nickel => { 5 }
+        Coin::Dime => { 10 }
+        Coin::Quarter(state) => {
+            println!("State from {:?}", state);
+            25
+        }
+    }
+}
+
+
+fn main() {
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+}
